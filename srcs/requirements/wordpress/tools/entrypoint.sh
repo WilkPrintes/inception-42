7a0f5e70cc4ebd41dbf700e438d9fadb9f609440
user42 <wilkp90@gmail.com>
2024-02-04 16:16:03 -0300

    change wordpress theme

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,31 +1,56 @@
+#!/bin/bash
+
+# Verifica se o arquivo de configuração do WordPress não existe
 if [ ! -f "/var/www/html/wp-config.php" ]; then
 
-        rm -rf *.*
+    # Remove todos os arquivos no diretório
+    rm -rf /var/www/html/*
 
-        wp core download --allow-root
-        
-        mv wp-config-sample.php wp-config.php
+    # Baixa o WordPress
+    wp core download --allow-root
+    
+    # Renomeia o arquivo de configuração
+    mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
-        sed -i "s/username_here/$WP_ADMIN_USER/g" wp-config.php
-	sed -i "s/password_here/$WP_ADMIN_PWD/g" wp-config.php
-	sed -i "s/localhost/$WP_HOST/g" wp-config.php
+    # Substitui as configurações no arquivo wp-config.php
+    sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/html/wp-config.php
+    sed -i "s/username_here/$WP_ADMIN_USER/g" /var/www/html/wp-config.php
+    sed -i "s/password_here/$WP_ADMIN_PWD/g" /var/www/html/wp-config.php
+    sed -i "s/localhost/$WP_HOST/g" /var/www/html/wp-config.php
 
-        wp core install --allow-root \
-            --url=$DOMAIN_NAME \
-            --title="Inception" \
-            --admin_user=$WP_ADMIN_USER \
-            --admin_email=$WP_ADMIN_EMAIL \
-            --admin_password=$WP_ADMIN_PWD \
-            --skip-email
+    # Instala o WordPress
+    wp core install --allow-root \
+        --url=$DOMAIN_NAME \
+        --title="Inception" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --admin_password=$WP_ADMIN_PWD \
+        --skip-email
 
-        wp user create --allow-root --user_login=$WP_USER --user_email=$WP_EMAIL --role=author --user_pass=$WP_PASSWORD
+    # Ativa o tema Astra
+    wp theme install astra --allow-root
+    wp theme activate astra --allow-root
+    wp theme delete twentytwentyfour --allow-root
+    wp theme delete twentytwentythree --allow-root
+    wp theme delete twentytwentytwo --allow-root
 
-        wp plugin uninstall akismet hello --allow-root
-        wp plugin update --all --allow-root
+    # Cria um usuário
+    wp user create --allow-root \
+        --user_login=$WP_USER \
+        --user_email=$WP_EMAIL \
+        --role=author \
+        --user_pass=$WP_PASSWORD
 
-        chown -R www-data:www-data /var/www/html
+    # Desinstala plugins padrão
+    wp plugin uninstall akismet hello --allow-root
+
+    # Atualiza todos os plugins
+    wp plugin update --all --allow-root
+
+    # Ajusta as permissões
+    chown -R www-data:www-data /var/www/html
 
 fi
 
+# Executa o PHP-FPM
 exec php-fpm7.4 -F
